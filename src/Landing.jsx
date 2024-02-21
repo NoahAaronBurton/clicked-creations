bfdde91f5903f8a7fc6cb1681b154e881a76919a
Noah Burton <nburton115@gmail.com>
2024-02-21 16:15:05 -0700

    adjusted img assets refs

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,16 +1,19 @@
 import Button from './components/Button';
 import InfoCard from './components/InfoCard';
 
+import backgroundImage from './assets/grid-mist.png';
+import gearImage from './assets/ai-click-gear-yellow.png';
+
 export default function Landing() {   
-    const backgroundImage =  "url('src/assets/grid-mist.png')";
+    // const backgroundImage =  "url('src/assets/grid-mist.png')";
 
     function goHome(e) {
         e.preventDefault();
         window.location.href = "/home";
     }
     return (
-        <div className="flex flex-col w-full overflow-y-auto bg-fixed bg-overlay text-white p-2 md:px-40" style={{backgroundImage}}>
-            <div id="jumbotron" className="flex flex-col px-2 h-full w-full items-center justify-center">
+        <div className="flex flex-col w-full overflow-y-auto text-white bg-fixed bg-overlay" style={{backgroundImage: `url(${backgroundImage})`}}>
+            <div className="flex flex-col w-full overflow-y-auto   p-2 md:px-40" >
                 <div className="flex justify-between items-center w-full mb-6 md:mb-20">
                     <h1 className="sm:text-2xl md:text-4xl p-4">
                         <strong>Clicked</strong> Creations
@@ -28,20 +31,20 @@ export default function Landing() {
                         </p>
                     </div>
                 </div>
-                    <div className='justify-center'>
+                    <div className='flex flex-col items-center justify-center'>
                         <p className=" text-2xl md:text-4xl text-balance text-center  mt-4 mb-4 underline decoration-solid decoration-orange-300">
                             Marketing Your Product Online Has Never Been Easier
                         </p>
+                        <button className="p-4 max-w-xs  rounded-lg bg-gradient-to-l from-orange-500 to-red-500 " onClick={goHome}>Try it out</button>
                     </div>
-                <button className="p-4 rounded-lg bg-gradient-to-l from-orange-500 to-red-500 " onClick={goHome}>Try it out</button>
             </div>
             
                 <div className="flex flex-col items-center justify-center w-full h-full mt-6">
                     <h2 className="text-4xl text-center">Our Services</h2>
                     <div className="flex flex-col md:flex-row w-full justify-center items-center">
-                        <InfoCard imageSrc="src/assets/ai-click-gear-yellow.png" header="Web Design" info="We can design a website for you that will look great on any device. We can also help you with SEO and social media." />
-                        <InfoCard imageSrc="src/assets/ai-click-gear-yellow.png" header="Branding" info="We can help you create a brand that will stand out from the competition. We can also help you with logo design and more." />
-                        <InfoCard imageSrc="src/assets/ai-click-gear-yellow.png" header="Social Media" info="We can help you create content for your social media accounts. We can also help you with social media marketing." />
+                        <InfoCard imageSrc={gearImage} header="Web Design" info="We can design a website for you that will look great on any device. We can also help you with SEO and social media." />
+                        <InfoCard imageSrc={gearImage} header="Branding" info="We can help you create a brand that will stand out from the competition. We can also help you with logo design and more." />
+                        <InfoCard imageSrc={gearImage} header="Social Media" info="We can help you create content for your social media accounts. We can also help you with social media marketing." />
                     </div>
                 </div>
            
