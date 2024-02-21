7ca188465b7a806b1629435173649b705a9cdea9
Noah Burton <nburton115@gmail.com>
2024-02-20 18:57:29 -0700

    Refactor Landing.jsx and add Button component

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,27 +1,32 @@
-export default function Landing() {
+import Button from './components/Button';
+
+export default function Landing() {   
     function goHome(e) {
         e.preventDefault();
         window.location.href = "/home";
     }
     return (
-        <div className="flex flex-col justify-items-center w-full overflow-y-auto text-black">
-            <div id="jumbotron" className="flex flex-col h-full w-full items-center justify-center bg-gradient-to-b from-primary to-[#27272A]">
-                <div className="flex flex-col text-balance items-center ">
-                    <h1 className="sm:text-4xl md:text-6xl mb-6 md:mb-20">
-                        Clicked Creations
-                    </h1>
-                </div>
-                <div className="flex flex-col justify-center md:flex-row md:w-3/4">
-                    <div className="flex-1">
-                        <p className="text-2xl md:text-4xl text-pretty">HELPING YOUR BRAND GROW WITH ARTIFICIAL INTELLIGENCE</p>
+        <div className="flex flex-col w-full overflow-y-auto">
+            <div id="jumbotron" className="flex flex-col px-2 h-full w-full items-center justify-center text-black bg-gradient-to-b from-primary to-[#27272A]">
+            <div className="flex justify-between items-center w-full ">
+                <h1 className="sm:text-4xl md:text-6xl mb-6 md:mb-20">
+                    Clicked Creations
+                </h1>
+                <Button onClick={goHome}>
+                    Login or Sign Up
+                </Button>
+            </div>
+                <div className="flex flex-col justify-center md:flex-row ">
+                    <div className="flex-1 space-y-8">
+                        <p className="text-2xl md:text-4xl text-pretty mb-12">HELPING YOUR BRAND GROW WITH ARTIFICIAL INTELLIGENCE</p>
                         <p className="text-lg text-pretty">
                             Web Design, Branding, SEO, Social Media Content, Creating Video & Photos, Blog Posting, Avatar Creation etc. If its digital we can create it!
                         </p>
+                        <button className="p-4 rounded-lg bg-gradient-to-l from-orange-500 to-red-500 " onClick={goHome}>Try it out</button>
                     </div>
-                    <img className="order-1 flex-1 w-full max-w-xs max-h-xs" src="src/assets/ai-click-gear-yellow.png" />
+                    <img className="flex-1 w-full max-w-xs max-h-xs" src="src/assets/ai-click-gear-yellow.png" />
                 </div>
             </div>
-            <button onClick={goHome}>Try it out</button>
         </div>
     )
 }
